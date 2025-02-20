502ca85744fb812b6d6b3c2b114c9c836196ce9a
Shumy <alexutushumy@gmail.com>
2025-02-20 10:04:44 +0200

    Fixed google maps API

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -24,24 +24,27 @@ export default function Contact() {
                     <p className="text-gray-800 mb-4">
                         We would love to hear from you! Whether you have a question about our research, events, or anything else, our team is ready to answer all your questions.
                     </p>
-                    <iframe
-                        src="https://forms.office.com/Pages/ResponsePage.aspx?id=-nnrpqnEzkyBjbhSdNFTBXfygrOv6LlPruqY6PJRcsFURDdWSVMxNVdRVjhNSEFQMVdXQ0UzNlo0Ti4u&origin=QRCode"
-                        width="100%"
-                        height="500"
-                        frameBorder="0"
-                        marginHeight="0"
-                        marginWidth="0"
-                        title="Contact Us Form"
-                        allowFullScreen
-                    ></iframe>
+                    <div style={{ overflow: 'hidden', height: '450px' }}>
+                        <iframe
+                            src="https://forms.office.com/Pages/ResponsePage.aspx?id=-nnrpqnEzkyBjbhSdNFTBXfygrOv6LlPruqY6PJRcsFURDdWSVMxNVdRVjhNSEFQMVdXQ0UzNlo0Ti4u&origin=QRCode"
+                            width="100%"
+                            height="500px"
+                            style={{ marginTop: '-50px' }}
+                            frameBorder="0"
+                            marginHeight="0"
+                            marginWidth="0"
+                            title="Contact Us Form"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
                 </section>
                 <section>
                     <h2 className="text-2xl font-semibold mb-4 text-yellow-600">Our Location</h2>
                     <p className="text-gray-800 mb-4">
-                        Adresa: Observatorului 2, Cluj-Napoca, 400489, Cluj-Napoca, Romania
+                        Adresa: Strada Observatorului 2, Cluj-Napoca 400347, Romania
                     </p>
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2719.567015612309!2d23.58383631560941!3d46.76937997913765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47490c2a6e4b8f4b%3A0x2e2b1b1e1e1e1e1e!2sObservatorului%202%2C%20Cluj-Napoca%20400489%2C%20Romania!5e0!3m2!1sen!2sus!4v1634567890123!5m2!1sen!2sus"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1366.6963107204735!2d23.595919670551666!3d46.757156971379665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47490c32b03140c1%3A0x437ed6aee538f132!2sStrada%20Observatorului%202%2C%20Cluj-Napoca%20400347!5e0!3m2!1sen!2sro!4v1740037763187!5m2!1sen!2sro"
                         width="100%"
                         height="450"
                         style={{ border: 0 }}
