b752abdc5cd506779b3ccc486fa161ad4bfee355
Shumy <alexutushumy@gmail.com>
2025-03-28 13:49:36 +0200

    feat: colaborators tab

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,8 +32,8 @@ export default function Navbar() {
           <li><Link href="/news" className="hover:underline">News</Link></li>
           <li><Link href="/media" className="hover:underline">Media</Link></li>
           <li><Link href="/timeline" className="hover:underline">Timeline</Link></li>
-          <li><Link href="/contact" className="hover:underline">Contact</Link></li>
           <li><Link href="/collaborators" className="hover:underline">Collaborators</Link></li>
+          <li><Link href="/contact" className="hover:underline">Contact</Link></li>
         </ul>
       </div>
 
@@ -45,8 +45,8 @@ export default function Navbar() {
           <li><Link href="/news" className="hover:underline" onClick={() => setIsOpen(false)}>News</Link></li>
           <li><Link href="/media" className="hover:underline" onClick={() => setIsOpen(false)}>Media</Link></li>
           <li><Link href="/timeline" className="hover:underline" onClick={() => setIsOpen(false)}>Timeline</Link></li>
-          <li><Link href="/contact" className="hover:underline" onClick={() => setIsOpen(false)}>Contact</Link></li>
           <li><Link href="/collaborators" className="hover:underline" onClick={() => setIsOpen(false)}>Collaborators</Link></li>
+          <li><Link href="/contact" className="hover:underline" onClick={() => setIsOpen(false)}>Contact</Link></li>
         </ul>
       )}
     </nav>
