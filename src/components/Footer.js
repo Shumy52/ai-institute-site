025efec60f0b6f6de354606e0a049cf6136d3373
Shumy52 <alexutushumy@gmail.com>
2025-06-27 12:59:10 +0300

    fix: footer privacy link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,7 @@ export default function Footer() {
     <footer className="bg-light-blue text-black p-6 text-center mt-12 border-t-2 border-gray-300">
       <p>© 2025 Technical University of Cluj-Napoca. All rights reserved.</p>
       <p className="mt-2">
-        <a href="#" className="underline">Privacy Policy</a> | 
-        <a href="#" className="underline">Info</a>
+        <a href="https://www.utcluj.ro/termeni-si-conditii/" className="underline">Privacy Policy</a>
       </p>
     </footer>
   );
