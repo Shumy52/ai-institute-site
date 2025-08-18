3b2a08d5008d4f911988087ca12b0ddf69aa3d1e
unknown <Mocan.Da.Alex@student.utcluj.ro>
2025-08-18 17:01:52 +0300

    navbar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect,  useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { FaBars, FaTimes } from 'react-icons/fa';
