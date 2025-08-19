2267bf7311c8ff3715c2c2e99fd73871b87062f0
unknown <iliesiu.robert13@gmail.com>
2025-08-19 15:40:17 +0300

    Added Themes page for Research functionality

---
+++ src/app/research/themes/page.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "ICIA - Research Themes",
+};
+
+import ThemesClient from "./themesClient";
+
+export default function ThemesPage() {
+  return <ThemesClient />;
+}
