8d6be1720485a418f10e64c94b0b31d51ada1ed3
Shumy52 <alexutushumy@gmail.com>
2026-01-12 19:44:51 +0200

    fix: server.ts prod issues

--- server/config/server.ts
+++ server/config/server.ts
@@ -1,14 +1,16 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  // This is the only one that matters
-  url: env('STRAPI_URL', ''), 
+  // 1. The public URL must be absolute and include the subpath
+  url: 'https://airi.utcluj.ro/strapi',
+  // 2. DELETE the 'admin' block entirely.
+  // Strapi will automatically append '/admin' to the url above.
   app: {
     keys: env.array('APP_KEYS'),
   },
   vite: {
     server: {
-      allowedHosts: ['solid.utcluj.ro'],
+      allowedHosts: ['solid.utcluj.ro', 'airi.utcluj.ro'],
     },
   },
 });
